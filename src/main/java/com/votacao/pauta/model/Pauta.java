b539072002b70c2cb0ac363b20247639ebc66ad5
Marcossjb1 <marcosviniciussjb@outlook.com>
2023-07-07 22:45:52 -0300

    Métodos implementados na Controller e novos Atributos na Model

--- src/main/java/com/votacao/pauta/model/Pauta.java
+++ src/main/java/com/votacao/pauta/model/Pauta.java
@@ -1,9 +1,15 @@
 package com.votacao.pauta.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 import java.util.Date;
 
 public class Pauta {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String descricao;
     private Date prazo;
 }
--- src/main/java/com/votacao/pauta/model/Voto.java
+++ src/main/java/com/votacao/pauta/model/Voto.java
@@ -1,8 +1,14 @@
 package com.votacao.pauta.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 public class Voto {
-    private long id;
-    private long idPauta;
-    private long idUsuario;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long idPauta;
+    private Long idUsuario;
     private boolean voto;
 }
