122e430a738d9aed85df902f89a22efc0445278e
unknown <ruchellguilherme100@gmail.com>
2023-07-23 17:20:05 -0300

    removendo yml

--- src/main/java/com/votacao/pauta/service/VotoService.java
+++ src/main/java/com/votacao/pauta/service/VotoService.java
@@ -19,18 +19,17 @@ public class VotoService {
     }
 
     public Voto inserirVoto(Voto voto) {
-        Optional<Pauta> pauta=pautaRepository.findById(voto.getIdPauta());
+
+        //Na linha abaixo estamos indo no banco de dados e vendo se o id da pauta existe
+        //Se a pauta existir, salva o voto, se não mostramos uma mensagem de erro na tela
         if (pautaRepository.existsById(voto.getIdPauta())){
             return votoRepository.save(voto);
         }
         throw new RuntimeException("Pauta não encontrada na base de dados");
     }
-    //TODO: USUÁRIO NÃO PODE INSERIR VOTO EM UMA PAUTA INEXISTENTE
-    //TODO: USUÁRIO NÃO PODE VOTAR MAIS DE UMA VEZ
-    //TODO: USUÁRIO NÃO PODE VOTAR SE A PAUTA ESTIVER FECHADA
-    //TODO: USUÁRIO NÃO PODE VOTAR SE ELE NÃO ESTIVER CRIADO NA BASE DE DADOS
 
     public Voto buscarVoto(Long id) {
+        //Na linha abaixo estamos indo no banco de dados e buscando o voto por id
         Optional<Voto> voto = votoRepository.findById(id);
         if (voto.isPresent()) {
             return voto.get();
@@ -38,3 +37,8 @@ public class VotoService {
         throw new RuntimeException("Não foi possível encontrar o voto na base de dados.");
     }
 }
+
+//TODO: USUÁRIO NÃO PODE INSERIR VOTO EM UMA PAUTA INEXISTENTE
+//TODO: USUÁRIO NÃO PODE VOTAR MAIS DE UMA VEZ
+//TODO: USUÁRIO NÃO PODE VOTAR SE A PAUTA ESTIVER FECHADA
+//TODO: USUÁRIO NÃO PODE VOTAR SE ELE NÃO ESTIVER CRIADO NA BASE DE DADOS
