a2208f958db13dd5a05e1676349ff168ceb323bf
Marcossjb1 <marcosviniciussjb@outlook.com>
2023-07-24 23:11:12 -0300

    Foi implemtada novas regras de negócio para a classe de serviço VotoService

--- src/main/java/com/votacao/pauta/service/VotoService.java
+++ src/main/java/com/votacao/pauta/service/VotoService.java
@@ -3,6 +3,7 @@ package com.votacao.pauta.service;
 import com.votacao.pauta.model.Pauta;
 import com.votacao.pauta.model.Voto;
 import com.votacao.pauta.repository.PautaRepository;
+import com.votacao.pauta.repository.UsuarioRepository;
 import com.votacao.pauta.repository.VotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,15 +17,21 @@ public class VotoService {
     private VotoRepository votoRepository;
     @Autowired
     private PautaRepository pautaRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     public Voto inserirVoto(Voto voto) {
 
         //Na linha abaixo estamos indo no banco de dados e vendo se o id da pauta existe
         //Se a pauta existir, salva o voto, se não mostramos uma mensagem de erro na tela
-        if (pautaRepository.existsById(voto.getIdPauta())){
-            return votoRepository.save(voto);
+        if (pautaRepository.existsById(voto.getIdPauta()) && usuarioRepository.existsById(voto.getIdUsuario())) {
+            Voto votos = votoRepository.findByIdUsuarioAndIdPauta(voto.getIdUsuario(), voto.getIdPauta());
+            if (votos == null) {
+                return votoRepository.save(voto);
+            }
+            throw new RuntimeException("Você não pode votar novamente.");
         }
-        throw new RuntimeException("Pauta não encontrada na base de dados");
+        throw new RuntimeException("Dados incorretos, para votar é preciso que o Usuário e Pauta estejam corretos.");
     }
 
     public Voto buscarVoto(Long id) {
@@ -37,7 +44,7 @@ public class VotoService {
     }
 }
 
-//TODO: USUÁRIO NÃO PODE INSERIR VOTO EM UMA PAUTA INEXISTENTE
-//TODO: USUÁRIO NÃO PODE VOTAR MAIS DE UMA VEZ
+//TODO: USUÁRIO NÃO PODE INSERIR VOTO EM UMA PAUTA INEXISTENTE [X]
+//TODO: USUÁRIO NÃO PODE VOTAR MAIS DE UMA VEZ [X]
 //TODO: USUÁRIO NÃO PODE VOTAR SE A PAUTA ESTIVER FECHADA
-//TODO: USUÁRIO NÃO PODE VOTAR SE ELE NÃO ESTIVER CRIADO NA BASE DE DADOS
+//TODO: USUÁRIO NÃO PODE VOTAR SE ELE NÃO ESTIVER CRIADO NA BASE DE DADOS [X]
