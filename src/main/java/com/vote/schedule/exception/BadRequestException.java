d8cd6fecbefbfb70ba349bcdc9100a46d33a3e64
Marcossjb1 <marcosviniciussjb@outlook.com>
2023-09-06 23:19:11 -0300

    Implementado novos testes unitários para a funcao createUser do UserServiceImpl

--- src/main/java/com/vote/schedule/exception/BadRequestException.java
+++ src/main/java/com/vote/schedule/exception/BadRequestException.java
@@ -1,5 +1,8 @@
 package com.vote.schedule.exception;
 
+import lombok.Getter;
+
+@Getter
 public class BadRequestException extends RuntimeException {
 
   public BadRequestException(String message) {
