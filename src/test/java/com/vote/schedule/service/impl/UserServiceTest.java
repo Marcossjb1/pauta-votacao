2a77124cdc7fd017d20d3cf2866fafb684139c34
Marcossjb1 <marcosviniciussjb@outlook.com>
2023-09-12 23:04:32 -0300

    Foi implementado novos testes únitários para a classe de UserServiceImpl

--- src/test/java/com/vote/schedule/service/impl/UserServiceTest.java
+++ src/test/java/com/vote/schedule/service/impl/UserServiceTest.java
@@ -4,23 +4,26 @@ import com.vote.schedule.exception.BadRequestException;
 import com.vote.schedule.model.User;
 import com.vote.schedule.repository.UserRepository;
 import com.vote.schedule.validation.UserValidator;
+import org.hibernate.ObjectNotFoundException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 
-    @Mock //MOCKAR UMA CLASSE É UTILIZAR DADOS POR DEFAULT SEM UTILIZAR OS DADOS REAIS
+    @Mock
     private UserRepository userRepository;
     @Mock
     private UserValidator userValidator;
@@ -29,7 +32,6 @@ public class UserServiceTest {
 
     @Test
     public void shouldCreateUser() {
-        //TESTE UNITÁRIO DE DADOS INSERIDOS NA FUNÇÃO CRIAR USUARIO
         var user = new User();
         user.setId(1L);
         user.setName("Marcos");
@@ -38,13 +40,14 @@ public class UserServiceTest {
         then(result.getId()).isEqualTo(1L);
         then(result.getName()).isEqualTo("Marcos");
     }
+
     @Test
     public void shouldReturnErrorWhenUserNameInvalid() {
-        //TESTE UNITÁRIO DE DADOS NÃO INSERIDOS PARA O NOME DE USUÁRIO
         var user = new User();
         user.setId(1L);
         user.setName("");
-        thenThrownBy(() -> userService.createUser(user)).isInstanceOf(BadRequestException.class);
+        thenThrownBy(() -> userService.createUser(user))
+                .isInstanceOf(BadRequestException.class);
     }
 
     @Test
@@ -57,5 +60,56 @@ public class UserServiceTest {
         then(result.getId()).isEqualTo(1L);
         then(result.getName()).isEqualTo("Gui");
     }
+
+    @Test
+    public void shouldReturnErrorWhenUserNotExist(){
+        thenThrownBy(() -> userService.searchUser(null))
+                .isInstanceOf(ObjectNotFoundException.class);
+    }
+
+    @Test
+    public void shouldReturnErrorForUserDeleted(){
+        thenThrownBy(() -> userService.deleteUser(null))
+                .isInstanceOf(ObjectNotFoundException.class);
+    }
+
+    @Test
+    public void shouldDeletedUser(){
+        var user = new User();
+        user.setName("Marcos");
+        user.setId(1L);
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        userService.deleteUser(1L);
+        verify(userRepository).deleteById(1L);
+    }
+
+    @Test
+    public void shouldListUsers(){
+        var userOne = new User();
+        var userTwo = new User();
+        var listUsers = List.of(userOne,userTwo);
+
+        userOne.setId(1L);
+        userOne.setName("Mailão");
+        userTwo.setId(2L);
+        userTwo.setName("Gui");
+
+        given(userRepository.findAll()).willReturn(listUsers);
+
+        var result = userService.listAllUsers();
+
+        then(result.get(0).getId()).isEqualTo(1L);
+        then(result.get(0).getName()).isEqualTo("Mailão");
+
+        then(result.get(1).getId()).isEqualTo(2L);
+        then(result.get(1).getName()).isEqualTo("Gui");
+    }
+
+    @Test
+    public void shouldReturnErrorUpdateUser(){
+        thenThrownBy(() -> userService.updateUser(null,null))
+                .isInstanceOf(ObjectNotFoundException.class);
+    }
+
 }
-//TODO: CRIAR TESTE PARA VALIDAR O CENÁRIO DE ERRO AO TENTAR RETORNAR UM USUÁRIO.
+
