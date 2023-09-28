032670dbf75c1b2594534904ef398b1732053227
Marcossjb1 <marcosviniciussjb@outlook.com>
2023-09-27 23:02:46 -0300

    Realizado a cobertura de teste unitário para o método createSchedule da classe ScheduleServiceImpl

--- src/test/java/com/vote/schedule/service/impl/ScheduleServiceTest.java
+++ src/test/java/com/vote/schedule/service/impl/ScheduleServiceTest.java
@@ -1,11 +1,18 @@
 package com.vote.schedule.service.impl;
 
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.BDDMockito.given;
+
+import com.vote.schedule.model.Schedule;
 import com.vote.schedule.repository.ScheduleRepository;
 import com.vote.schedule.validation.ScheduleValidator;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDateTime;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ScheduleServiceTest {
@@ -16,5 +23,24 @@ public class ScheduleServiceTest {
     private ScheduleValidator scheduleValidator;
     @InjectMocks
     private ScheduleServiceImpl scheduleService;
+
+    @Test
+    public void shouldReturnCreateSchedule(){
+        var schedule = new Schedule();
+        var date = LocalDateTime.now();
+
+        schedule.setId(1L);
+        schedule.setDescription("Teste unitário de Pauta");
+        schedule.setDeadline(date);
+
+        given(scheduleRepository.save(schedule)).willReturn(schedule);
+
+        var result = scheduleService.createSchedule(schedule);
+
+        then(result.getId()).isEqualTo(1L);
+        then(result.getDescription()).isEqualTo("Teste unitário de Pauta");
+        then(result.getDeadline()).isEqualTo(date);
+
+    }
 
 }
