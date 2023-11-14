fafcf4395d83fd21e29ccac8bebf09c7d79808eb
unknown <ruchellguilherme100@gmail.com>
2023-11-14 15:55:59 -0300

    removendo teste não utilizado

--- src/test/java/com/vote/schedule/controller/ScheduleControllerIT.java
+++ src/test/java/com/vote/schedule/controller/ScheduleControllerIT.java
@@ -1,19 +1,7 @@
 package com.vote.schedule.controller;
 
-import static io.restassured.RestAssured.given;
-
 import com.vote.schedule.config.IntegrationTest;
-import org.junit.Test;
 
 public class ScheduleControllerIT extends IntegrationTest {
 
-  @Test
-  public void getUser() {
-    given()
-        .basePath("/user")
-        .when()
-        .get("/list-users")
-        .then()
-        .statusCode(200);
-  }
 }
