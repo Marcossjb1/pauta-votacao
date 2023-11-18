850c811e1c905741e2480f3d43068887804624bd
Marcossjb1 <marcosviniciussjb@outlook.com>
2023-11-17 23:20:15 -0300

    Implementado teste de integração para createUser

--- src/test/java/com/vote/schedule/controller/UserControllerIT.java
+++ src/test/java/com/vote/schedule/controller/UserControllerIT.java
@@ -3,8 +3,10 @@ package com.vote.schedule.controller;
 import com.vote.schedule.config.IntegrationTest;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static com.vote.schedule.fixture.UserFixture.getUser;
 import static com.vote.schedule.fixture.UserFixture.postUser;
+import static com.vote.schedule.random.Random.firstName;
 import static com.vote.schedule.utils.BodyBuilder.with;
 
 public class UserControllerIT extends IntegrationTest {
@@ -18,12 +20,16 @@ public class UserControllerIT extends IntegrationTest {
 
     @Test
     public void shouldCreateUser() {
+        var name = firstName();
+
         var userBody = with("id", 1L)
-                .and("name", "marcos");
+                .and("name", name);
 
         postUser(userBody)
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("id", is(1))
+                .body("name", is(name));
     }
 }
 
